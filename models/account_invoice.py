0866cd3cfed4af8ab3a6f088021207be9add4c12
root <root@paginas11.us-central1-a.c.nth-cumulus-316014.internal>
2022-11-10 06:39:00 +0000

    ajuste a impresion

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -496,8 +496,8 @@ class AccountMove(models.Model):
                             import base64 
                             print(final_data)
                             if final_data['code'] == '400':
-                                return self.env['wk.wizard.message'].genrated_message('Estamos recibiendo un codigo 400 Es necesario esperar para volver imprimir el documento', 'Es necesario esperar para volver a imprimir el documento')
-                            elif final_data['code'] == '201':
+                                return self.env['wk.wizard.message'].genrated_message(final_data['code'], final_data)
+                            elif final_data['code'] == '201' or final_data['code'] == '200':
                                 print("el codigo")
                                 print(final_data['code'])
                                 image_64_encode = base64.b64decode(final_data['documentBase64']) #eval(
@@ -525,7 +525,7 @@ class AccountMove(models.Model):
                                     self.write({"impreso":True})
                                     return self.env['wk.wizard.message'].genrated_message("Ve a attachment","Factura impresa" ,"https://navegasoft.com")
                             else:
-                                return self.env['wk.wizard.message'].genrated_message('Estamos recibiendo un codigo de error Es necesario esperar para volver imprimir el documento', 'Es necesario esperar para volver a imprimir el documento')
+                                return self.env['wk.wizard.message'].genrated_message(final_data['code'], final_data)
                                 
                     else:
                         raise UserError(_('Ve a attachment, Factura ya impresa.'))
