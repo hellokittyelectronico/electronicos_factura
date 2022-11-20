75f023004f4e82bbb79538db71b46488266a961e
root <root@DESKTOP-4PIFAC9.localdomain>
2022-11-20 15:26:58 +0100

    ajuste para que suba

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import account_invoice
-from . import models
-from . import res_partner
-from . import ir_sequence
-from . import account_journal
+# from . import account_invoice
+# from . import models
+# from . import res_partner
+# from . import ir_sequence
+# from . import account_journal
