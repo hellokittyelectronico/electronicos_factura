4f4c9853a5be3dfd2a20f348d641aaeeac6b6adc
root <root@DESKTOP-4PIFAC9.localdomain>
2022-12-03 14:22:46 +0100

    sacar mensaje de error al pedir cufe

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -621,7 +621,10 @@ class AccountMove(models.Model):
                     send = {"id_plataforma":self.company_id.partner_id.id_plataforma,"password":self.company_id.partner_id.password,"prefijo":prefijo,"folio":folio,"tipo_documento":"cufe","documento_electronico":"factura","tipo_documento2":self.tipo_documento}
                     cufe = self.pedircufe(send,urlini)
                     print(cufe)
-                    self.factura.write({"cufe":cufe['cufe']})
+                    if 'cufe' in cufe:
+                        self.factura.write({"cufe":cufe['cufe']})
+                    else:
+                        return self.env['wk.wizard.message'].genrated_message("2 "+cufe, "Error pidiendo el cufe","http://navegasoft.com")
                     # return
                     #self.write({"cufe":})
             send,error = invoice.to_json()
