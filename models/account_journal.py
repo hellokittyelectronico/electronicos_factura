5cb8cd7a060732cca43129a4b9477879a1513364
root <root@DESKTOP-4PIFAC9.localdomain>
2022-12-21 09:10:35 +0100

    agregar resolucion a refund

--- models/account_journal.py
+++ models/account_journal.py
@@ -22,7 +22,7 @@ class AccountJournal(models.Model):
     )
     number_refund_from = fields.Integer('inicio refund', required=True)
     number_refund_to = fields.Integer('fin refund', required=True)
-    
+    resolution_refund_number = fields.Char("Resolucion refund numero") 
 
 
 
