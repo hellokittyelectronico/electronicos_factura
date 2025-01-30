119748f7fc596694b12b95445ec3a4c0b9326baf
root <root@DESKTOP-4PIFAC9.localdomain>
2025-01-30 11:34:52 +0100

    multimempresa

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -259,7 +259,7 @@ class AccountMove(models.Model):
         valores = self.env['base_electronicos.tabla'].search([('name', '=', 'Factura electrónica')])
         response2={}
         valores_lineas = valores.mp_id
-        documento = valores.general_factura.search([('diario', '=', self.journal_id[0].id),('company_id','=',self.company_id[0].id)])
+        documento = valores.general_factura.search([('diario', '=', self.journal_id[0].id),('company_id','=',self.company_id[0].id)],limit=1)
         # print("haber")
         # print(self.journal_id)
         # print(documento.tipo_factura)
@@ -269,8 +269,6 @@ class AccountMove(models.Model):
         # print(self[1].move_type)
         _logger.info(self[0].move_type)
         _logger.info(documento)
-        for nombre in documento.tipo_factura:
-            _logger.info(nombre.name)
         _logger.info(documento.tipo_factura[0])    
         _logger.info(self.journal_id[0].id)
         if documento:
