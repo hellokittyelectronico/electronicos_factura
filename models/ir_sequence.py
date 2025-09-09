881bed66a417d8e45419468680a0c266a875df08
BryanCando <156931713+bryancandonavegasoft@users.noreply.github.com>
2025-09-09 11:43:26 -0500

    Update ir_sequence.py

--- models/ir_sequence.py
+++ models/ir_sequence.py
@@ -33,6 +33,7 @@ def _update_nogap(self, number_increment):
     number_next = self.number_next
     self._cr.execute("SELECT number_next FROM %s WHERE id=%s FOR UPDATE NOWAIT" % (self._table, self.id))
     self._cr.execute("UPDATE %s SET number_next=number_next+%s WHERE id=%s " % (self._table, number_increment, self.id))
+    print('cambio')
     self._invalidate_cache(['number_next'], [self.id])
     return number_next
 
